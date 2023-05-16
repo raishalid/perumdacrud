40f73ef06564828fd2c65cb4e8d727c2ffc64d71
Muhammad Rais <mraisfx@gmail.com>
2023-05-16 19:54:12 +0800

    second commits

--- src/config.php
+++ src/config.php
@@ -142,7 +142,7 @@ $CONFIG = [
 
     // General
     "UNFORMAT_YEAR" => 50, // Unformat year
-    "RANDOM_KEY" => 'xwH4WD84fDvPannb', // Random key for encryption
+    "RANDOM_KEY" => 'lRuM8RNxYOXbgN8n', // Random key for encryption
     "ENCRYPTION_KEY" => '', // Encryption key for data protection
     "PROJECT_STYLESHEET_FILENAME" => "css/crudperumdautama.css", // Project stylesheet file name
     "USE_COMPRESSED_STYLESHEET" => true, // Compressed stylesheet
